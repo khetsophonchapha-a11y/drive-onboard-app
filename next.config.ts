9dad43aa6d6e5bc3564f5643c6b27efe500c7ead
khetsophon chapha <khetsophon.chapha@gmail.com>
2025-09-28 05:22:37 +0000

    Try fixing this error: `Runtime Error: Error: Invalid src prop (https://

--- next.config.ts
+++ next.config.ts
@@ -28,6 +28,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: '*.r2.cloudflarestorage.com',
+        port: '',
+        pathname: '/**',
+      }
     ],
   },
 };
